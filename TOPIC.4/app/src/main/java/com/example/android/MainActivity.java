9d7c3241b58e17d1992f7972defc048870d6a5f8
Jose Miguel Rivas Mendez <jrivas@cifpfbmoll.eu>
2020-11-09 10:35:20 +0100

    Adding TOPIC 4 Task12A

---
+++ TOPIC.4/app/src/main/java/com/example/android/MainActivity.java
@@ -0,0 +1,29 @@
+package com.example.android;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.res.Resources;
+import android.os.Bundle;
+import android.util.DisplayMetrics;
+import android.view.View;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+public int counter;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        counter=0;
+    }
+
+    public void increaseCounter(View view){
+    counter++;
+    showNumber();
+    }
+
+    public void showNumber(){
+        TextView textView = (TextView) findViewById(R.id.show_count);
+        textView.setText(counter);
+    }
+}
