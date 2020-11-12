0a28956968dadb372b68f1c4aaeec70435129a28
Jose Miguel Rivas Mendez <jrivas@cifpfbmoll.eu>
2020-11-12 18:10:13 +0100

    Adding Task14

---
+++ Lesson4_Task14/app/src/main/java/com/jrivas/lesson4_task14/MainActivity.java
@@ -0,0 +1,17 @@
+package com.jrivas.lesson4_task14;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.Bundle;
+import android.widget.ImageView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
