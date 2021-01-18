4b052af36c6035db8dd5d876098ac780f8e7f7ff
Jose Miguel Rivas Mendez <jrivas@cifpfbmoll.eu>
2021-01-18 21:58:22 +0100

    Scorekeeper finished.

--- TOPIC.8/ScoreKeepers/app/src/main/java/com/jrivas/scorekeeper/MainActivity.java
+++ TOPIC.8/ScoreKeepers/app/src/main/java/com/jrivas/scorekeeper/MainActivity.java
@@ -1,27 +1,82 @@
 package com.jrivas.scorekeeper;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatDelegate;
 
+import android.app.Activity;
+import android.content.Context;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-
+    String STATE_SCORE_1 = "Team 1 Score";
+    String STATE_SCORE_2 = "Team 2 Score";
     int mScore1;
     int mScore2;
     TextView mTextView1;
     TextView mTextView2;
+    Context mContext;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mContext = this;
         mScore1 = 0;
         mScore2 = 0;
         mTextView1 = findViewById(R.id.counter_team_1);
         mTextView2 = findViewById(R.id.counter_team_2);
+        if (savedInstanceState != null) {
+            mScore1 = savedInstanceState.getInt(STATE_SCORE_1);
+            mScore2 = savedInstanceState.getInt(STATE_SCORE_2);
+//Set the score text views
+            mTextView1.setText(String.valueOf(mScore1));
+            mTextView2.setText(String.valueOf(mScore2));
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+//Inflate the menu from XML
+        getMenuInflater().inflate(R.menu.main_menu, menu);
+//Change the label of the menu based on the state of the app
+        int nightMode = AppCompatDelegate.getDefaultNightMode();
+        if(nightMode == AppCompatDelegate.MODE_NIGHT_YES){
+            menu.findItem(R.id.night_mode).setTitle(R.string.day_mode);
+        } else{
+            menu.findItem(R.id.night_mode).setTitle(R.string.night_mode);
+        }
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+//Check if the correct item was clicked
+        if (item.getItemId() == R.id.night_mode) {
+            AppCompatDelegate.getDefaultNightMode();
+        }
+        //Get the night mode state of the app
+        int nightMode = AppCompatDelegate.getDefaultNightMode();
+//Set the theme mode for the restarted activity
+        if (nightMode == AppCompatDelegate.MODE_NIGHT_YES) {
+            AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
+        } else {
+            AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES);
+        }
+        return true;
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+//Save the scores
+        outState.putInt(STATE_SCORE_1, mScore1);
+        outState.putInt(STATE_SCORE_2, mScore2);
+        super.onSaveInstanceState(outState);
     }
 
     public void increaseScore(View view) {
