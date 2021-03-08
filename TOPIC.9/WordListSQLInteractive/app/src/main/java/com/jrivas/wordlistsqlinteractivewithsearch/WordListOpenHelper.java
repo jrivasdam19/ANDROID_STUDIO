ce741dcbdd0d19217daf2ba9d36935cb5ce636d7
Jose Miguel Rivas Mendez <jrivas@cifpfbmoll.eu>
2021-03-08 20:34:39 +0100

    kkk

--- TOPIC.9/WordListSQLInteractive/app/src/main/java/com/jrivas/wordlistsqlinteractivewithsearch/WordListOpenHelper.java
+++ TOPIC.9/WordListSQLInteractive/app/src/main/java/com/jrivas/wordlistsqlinteractivewithsearch/WordListOpenHelper.java
@@ -8,6 +8,10 @@ import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 import android.util.Log;
 
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+
 public class WordListOpenHelper extends SQLiteOpenHelper {
 
     private static final String TAG = WordListOpenHelper.class.getSimpleName();
